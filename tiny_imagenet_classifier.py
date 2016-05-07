f570256394c76bac4fd6cb94da3c134d79ac01d6
Stefan Dierauf <sdierauf@gmail.com>
2016-05-07 15:22:20 +0200

    w/e

--- tiny_imagenet_classifier.py
+++ tiny_imagenet_classifier.py
@@ -59,6 +59,9 @@ val_annotations_map = get_annotations_map()
 X_test = np.zeros([200*500,3,64,64],dtype='uint8')
 y_test = np.zeros([200*500], dtype='uint16')
 
+
+println('loading test images...')
+
 i = 0
 testPath='./tiny-imagenet-200/val/images'
 for sChild in os.listdir(testPath):
