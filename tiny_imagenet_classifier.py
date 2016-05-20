bcd9e870e331a8466838ed27ef630c34ed1b9922
Stefan Dierauf <sdierauf@gmail.com>
2016-05-20 12:56:00 +0000

    50

--- tiny_imagenet_classifier.py
+++ tiny_imagenet_classifier.py
@@ -29,7 +29,7 @@ from val_load import get_annotations_map
 loss_functions = ['categorical_crossentropy', 'hinge', 'squared_hinge']
 # num_classes_arr = [2, 5, 8]
 # num_classes_arr = [10, 100, 200]
-num_classes_arr = [5]
+num_classes_arr = [50]
 for loss_function in loss_functions:
     for num_classes in num_classes_arr: # num classes loop
 
